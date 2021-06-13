3c0e390b8af457443a733596aa064e337d6603ee
kin29 <kin29.com@gmail.com>
2021-06-13 23:02:03 +0900

    heroを選択したときもmessageがでるようにする

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../model/hero';
 import { HeroService } from '../service/hero.service';
+import { MessageService } from "../service/message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +11,7 @@ import { HeroService } from '../service/hero.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero? : Hero;
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -18,6 +19,7 @@ export class HeroesComponent implements OnInit {
 
   onSelected(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
