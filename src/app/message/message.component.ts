012c56befbac443f74c8aebe776f7aa572cfb6fc
kin29 <kin29.com@gmail.com>
2021-06-13 22:57:07 +0900

    MessageComponentでMessageServiceを使うようにしてmessageが表示されるようにする

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from "../service/message.service";
 
 @Component({
   selector: 'app-message',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
