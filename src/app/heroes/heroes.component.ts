74676b13b8dbb896b04d4233de05097fe244493a
kin29 <kin29.com@gmail.com>
2021-06-13 16:30:45 +0900

    選択されたヒーローを編集できるようにする

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,13 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero? : Hero;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelected(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
