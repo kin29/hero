f12c09ca6d6a72e3244bfe95d14ed73e0533edb6
kin29 <kin29.com@gmail.com>
2021-06-16 17:19:21 +0900

    Heroの名前で検索できるようにする

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from "rxjs";
+import { Hero } from "../model/hero";
+import { HeroService } from "../service/hero.service";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
 
 @Component({
   selector: 'app-hero-search',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-search.component.scss']
 })
 export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>; //heroes$ で Observableであることを宣言
+  private searchTerms = new Subject<string>()
+  constructor(private heroService: HeroService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // 検索語をobservableストリームにpushする
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // 各キーストロークの後、検索前に300ms待つ
+      debounceTime(300),
+      // 直前の検索語と同じ場合は無視する
+      distinctUntilChanged(),
+      // 検索語が変わる度に、新しい検索observableにスイッチする
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
 }
