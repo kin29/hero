cadb2dd4f7f0a508f683b08dc2290b4c1799b3f9
kin29 <kin29.com@gmail.com>
2021-06-16 15:01:31 +0900

    getHeroesの帰り値をof()→http.get()に変更

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -3,23 +3,29 @@ import { Hero } from "../model/hero";
 import { HEROES } from "../mock-heroes";
 import { Observable, of } from "rxjs";
 import { MessageService } from "./message.service";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes'
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService,
+  ) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
